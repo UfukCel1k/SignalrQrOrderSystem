5b38dab127718cf832d8cc1733e5fdb8bf4bc46a
Ufuk Çelik <ufukcelik.dev@gmail.com>
2024-03-30 22:52:32 +0300

    SignalrQrOrderSystem
    Layers, classes and packages were added to the project.

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoyID { get; set; }
+        public string CategoyName { get; set; }
+        public bool CategoyStatus { get; set; }
+    }
+}
